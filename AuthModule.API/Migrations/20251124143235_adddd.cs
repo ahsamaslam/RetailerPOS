4b8fa3b31dd45e9b8befcb256d3f1a7bcc1c6b37
Ahsam <syed.muhammad@snaile.com>
2025-11-26 19:15:04 +0500

    sdded files

---
+++ AuthModule.API/Migrations/20251124143235_adddd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthModule.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
